dda9b435a64c32e5ac6a2b07039eb7311d6d5308
clay austin <cgaustin@usgs.gov>
2015-12-23 20:25:24 +0000

    setup dns settings for docker container

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-iptables -t nat -F
-ifconfig docker0 down
-brctl delbr docker
-docker --dns 8.8.8.8 --dns 8.8.4.4 --dns 152.61.192.1 --dns 136.177.16.3 --dns-search cr.usgs.gov -d
 
+wai=`whoami`
+if [[ $wai != 'root' ]]
+  then
+    echo 'this script needs to be run as root'
+    exit 2
+fi
 
+echo 'this script is for a very specific use case. 
+Windows 7, running an Ubuntu 15.10 VirtualBox 
+instance, in order to allow Docker
+containers to connect to the interwebs'
 
+ip1='152.61.192.1'
+ip2='136.177.16.3'
+restart=False
+
+cd /etc
+for ip in $ip1 $ip2
+  do
+    if [[ -z `grep ${ip} resolv.conf` ]]
+      then
+        echo "nameserver $ip" >> resolv.conf
+        restart=True
+    fi
+done
+
+if [[ $restart == True ]]
+  then
+    echo "restart docker:  service docker restart ..."
+    service docker restart
+fi
